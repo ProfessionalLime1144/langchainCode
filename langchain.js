8ccef23f5428beb0cb77ca51cd1a9235628f29c4
ProfessionalLime1144 <102875598+ProfessionalLime1144@users.noreply.github.com>
2023-10-15 23:14:40 -0400

    Update langchain.js

--- langchain.js
+++ langchain.js
@@ -17,15 +17,8 @@ app.listen(3000, () => {
   console.log("Connected to Port 3000");
 });
 
-app.post("/", (req, res) => {
-  const input = req.get("input");
-  const destinationPath = req.get("destinationPath");
-  console.log(input);
-  console.log(destinationPath);
-  res.json({ input, destinationPath });
-});
 
-async function langchain(input, destinationPath) {
+app.get("/", (req, res) => {
   // Get PDF
   const loader = new PDFLoader(destinationPath);
   const pdfDoc = await loader.load();
@@ -44,6 +37,36 @@ async function langchain(input, destinationPath) {
   const knowledgeBase = await FaissStore.fromDocuments(chunks, new OpenAIEmbeddings("gpt-3.5-turbo", {
     openAIApiKey: process.env.OPENAI_API_KEY
   }));
+  req.locals.test = "test";
+  res.locals.knowledgeBase = knowledgeBase;
+})
+
+app.post("/", async (req, res) => {
+  const input = req.get("input");
+  const serverResponse = await langchain(input, req.sessions.knowledgeBase);
+  console.log("TEST" + req.locals.test);
+  res.json({ serverResponse });
+});
+
+async function langchain(input, knowledgeBase) {
+  // // Get PDF
+  // const loader = new PDFLoader(destinationPath);
+  // const pdfDoc = await loader.load();
+  // // Get only the text portion  
+  // let text = "";
+  // for (let i = 0; i < pdfDoc.length; i++) {
+  //   text += await pdfDoc[i].pageContent;
+  // }
+  // // chunks is an array
+  // const textSplitter = new CharacterTextSplitter({
+  //   separator: '\n',
+  //   chunkSize: 1000,
+  //   chunkOverlap: 200,
+  // });
+  // const chunks = await textSplitter.createDocuments([text]);
+  // const knowledgeBase = await FaissStore.fromDocuments(chunks, new OpenAIEmbeddings("gpt-3.5-turbo", {
+  //   openAIApiKey: process.env.OPENAI_API_KEY
+  // }));
   const docs = await knowledgeBase.similaritySearch(input);
   const llm = new OpenAI();
   const chain = await loadQAChain(llm, { type: "stuff" });
